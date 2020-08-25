41e6613a048e6dee88b2f40e71b3ce1a6b6cc90c
manishmalhotrawork <manish.malhotra.work@gmail.com>
2020-08-24 22:59:20 -0700

    Spark: Fix NPE while estimating statistics on an empty table (#1376)

--- spark2/src/main/java/org/apache/iceberg/spark/source/Reader.java
+++ spark2/src/main/java/org/apache/iceberg/spark/source/Reader.java
@@ -277,6 +277,11 @@ public class Reader implements DataSourceReader, SupportsScanColumnarBatch, Supp
 
   @Override
   public Statistics estimateStatistics() {
+    // its a fresh table, no data
+    if (table.currentSnapshot() == null) {
+      return new Stats(0L, 0L);
+    }
+
     if (filterExpressions == null || filterExpressions == Expressions.alwaysTrue()) {
       long totalRecords = PropertyUtil.propertyAsLong(table.currentSnapshot().summary(),
           SnapshotSummary.TOTAL_RECORDS_PROP, Long.MAX_VALUE);
