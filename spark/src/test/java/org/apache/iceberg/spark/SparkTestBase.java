19dfefbe7b82bbb626b3612dc31062bd1d7910e6
Kyle Bendickson <9833362+kbendick@users.noreply.github.com>
2020-09-30 15:58:17 -0700

    Spark: Add null check for catalog in SparkTestBase (#1529)

--- spark/src/test/java/org/apache/iceberg/spark/SparkTestBase.java
+++ spark/src/test/java/org/apache/iceberg/spark/SparkTestBase.java
@@ -63,7 +63,9 @@ public class SparkTestBase {
 
   @AfterClass
   public static void stopMetastoreAndSpark() {
-    catalog.close();
+    if (catalog != null) {
+      catalog.close();
+    }
     SparkTestBase.catalog = null;
     metastore.stop();
     SparkTestBase.metastore = null;
