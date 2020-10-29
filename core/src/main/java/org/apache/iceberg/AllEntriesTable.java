c136721bcbadf9f78b86666e52b36fb1665deb6f
Russell Spitzer <russell.spitzer@gmail.com>
2020-10-29 11:51:18 -0700

    Core: Fix entries metadata tables with delete manifests (#1673)
    (cherry picked from commit 43a7427874ab01c6811979b3b97e6c97fe074184)

--- core/src/main/java/org/apache/iceberg/AllEntriesTable.java
+++ core/src/main/java/org/apache/iceberg/AllEntriesTable.java
@@ -109,7 +109,7 @@ public class AllEntriesTable extends BaseMetadataTable {
       ResidualEvaluator residuals = ResidualEvaluator.unpartitioned(filter);
 
       return CloseableIterable.transform(manifests, manifest -> new ManifestEntriesTable.ManifestReadTask(
-          ops.io(), manifest, fileSchema, schemaString, specString, residuals));
+          ops.io(), manifest, fileSchema, schemaString, specString, residuals, ops.current().specsById()));
     }
   }
 
