811623613cc3c812c4c1b3c3b3e71a7e88c807c0
Chen, Junjie <chenjunjiedada@gmail.com>
2021-01-07 08:58:28 +0800

    Core: Fix projected transform expression names (#2027)

--- api/src/main/java/org/apache/iceberg/transforms/Timestamps.java
+++ api/src/main/java/org/apache/iceberg/transforms/Timestamps.java
@@ -76,7 +76,7 @@ enum Timestamps implements Transform<Long, Integer> {
   @Override
   public UnboundPredicate<Integer> project(String fieldName, BoundPredicate<Long> pred) {
     if (pred.term() instanceof BoundTransform) {
-      return ProjectionUtil.projectTransformPredicate(this, name, pred);
+      return ProjectionUtil.projectTransformPredicate(this, fieldName, pred);
     }
 
     if (pred.isUnaryPredicate()) {
@@ -92,7 +92,7 @@ enum Timestamps implements Transform<Long, Integer> {
   @Override
   public UnboundPredicate<Integer> projectStrict(String fieldName, BoundPredicate<Long> pred) {
     if (pred.term() instanceof BoundTransform) {
-      return ProjectionUtil.projectTransformPredicate(this, name, pred);
+      return ProjectionUtil.projectTransformPredicate(this, fieldName, pred);
     }
 
     if (pred.isUnaryPredicate()) {
