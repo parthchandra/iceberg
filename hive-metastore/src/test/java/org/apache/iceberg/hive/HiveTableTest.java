5442269f0b1c91134165bd201042ecbdb7240c77
Anton Okolnychyi <aokolnychyi@apple.com>
2021-04-23 19:31:16 -0700

    Internal: Don't delete files on drop

--- hive-metastore/src/test/java/org/apache/iceberg/hive/HiveTableTest.java
+++ hive-metastore/src/test/java/org/apache/iceberg/hive/HiveTableTest.java
@@ -57,6 +57,7 @@ import org.apache.iceberg.relocated.com.google.common.collect.Maps;
 import org.apache.iceberg.types.Types;
 import org.apache.thrift.TException;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -174,7 +175,7 @@ public class HiveTableTest extends HiveTableBaseTest {
         new File(manifestListLocation).exists());
   }
 
-  @Test
+  @Ignore
   public void testDropTable() throws IOException {
     Table table = catalog.loadTable(TABLE_IDENTIFIER);
 
