110e059d18781349e64f32bd4344b78608eed288
Anton Okolnychyi <aokolnychyi@apple.com>
2022-02-10 12:37:41 -0800

    Internal: Disable locality by default

--- spark/v3.2/spark/src/main/java/org/apache/iceberg/spark/SparkReadConf.java
+++ spark/v3.2/spark/src/main/java/org/apache/iceberg/spark/SparkReadConf.java
@@ -65,15 +65,17 @@ public class SparkReadConf {
   }
 
   public boolean localityEnabled() {
-    InputFile file = table.io().newInputFile(table.location());
+    boolean localityEnabled = confParser.booleanConf()
+        .option(SparkReadOptions.LOCALITY)
+        .defaultValue(false)
+        .parse();
 
-    if (file instanceof HadoopInputFile) {
-      String scheme = ((HadoopInputFile) file).getFileSystem().getScheme();
-      boolean defaultValue = LOCALITY_WHITELIST_FS.contains(scheme);
-      return PropertyUtil.propertyAsBoolean(
-          readOptions,
-          SparkReadOptions.LOCALITY,
-          defaultValue);
+    if (localityEnabled) {
+      InputFile in = table.io().newInputFile(table.location());
+      if (in instanceof HadoopInputFile) {
+        String scheme = ((HadoopInputFile) in).getFileSystem().getScheme();
+        return LOCALITY_WHITELIST_FS.contains(scheme);
+      }
     }
 
     return false;
