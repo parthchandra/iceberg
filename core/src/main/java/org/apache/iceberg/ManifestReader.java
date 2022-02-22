683dbc6fbd6bdd94f422ed3dc83bf581f69410c3
Yufei Gu <yufei_gu@apple.com>
2022-02-21 17:00:51 -0800

    Internal: DR actions (#555)
    * Internal: DR actions
    
    (cherry picked from commit c8b93e9cf61dac8a035a163e03608d308cc36b7b)
    
    * Internal: Replace the paths in table properties. (#503)
    
    (cherry picked from commit fd314851fc4f437379ecd13f553334d6d02cecf3)

--- core/src/main/java/org/apache/iceberg/ManifestReader.java
+++ core/src/main/java/org/apache/iceberg/ManifestReader.java
@@ -168,7 +168,7 @@ public class ManifestReader<F extends ContentFile<F>>
     return this;
   }
 
-  CloseableIterable<ManifestEntry<F>> entries() {
+  public CloseableIterable<ManifestEntry<F>> entries() {
     if ((rowFilter != null && rowFilter != Expressions.alwaysTrue()) ||
         (partFilter != null && partFilter != Expressions.alwaysTrue())) {
       Evaluator evaluator = evaluator();
