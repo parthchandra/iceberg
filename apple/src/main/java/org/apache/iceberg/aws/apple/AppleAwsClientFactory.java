e627bb8bdfc6c2fe23db6c2bc76ff22b1aa3a196
Russell Spitzer <rspitzer@apple.com>
2023-08-01 16:02:43 -0500

    Internal: Fix Scheme in AppleAwsClient Factory (#889) (#890) (#1042)
    Also adds tests and checks for Kryo Serialization

--- apple/src/main/java/org/apache/iceberg/aws/apple/AppleAwsClientFactory.java
+++ apple/src/main/java/org/apache/iceberg/aws/apple/AppleAwsClientFactory.java
@@ -22,12 +22,19 @@ import com.apple.awsappleconnect.java.credentials.AWSAppleCredentials;
 import com.apple.awsappleconnect.java.credentials.AWSAppleCredentialsProviderBase;
 import com.apple.awsappleconnect.java.provider.AWSAppleConnectCredentialsProvider;
 import com.apple.awsappleconnect.java.service.CredentialsConstants;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.KryoSerializable;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.aws.AwsClientFactory;
@@ -37,7 +44,8 @@ import org.apache.iceberg.relocated.com.google.common.annotations.VisibleForTest
 import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableSet;
-import org.apache.spark.sql.SparkSession$;
+import org.apache.iceberg.relocated.com.google.common.collect.Maps;
+import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
@@ -58,53 +66,23 @@ import software.amazon.awssdk.services.s3.S3Client;
  * provided by {@link AWSAppleConnectCredentialsProvider}. Configured using Iceberg Catalog
  * properties with keys identical to those required for <a
  * href="https://github.pie.apple.com/pie-spark/awsappleconnect-spark-utils">hadoop
- * configuration</a>. Interprets properties from Hadoop Configuration, Spark Configuration or
- * Catalog Configuration
+ * configuration</a>. Interprets properties from the Hadoop Configuration or Spark Configuration
+ * (spark.hadoop.*)
  */
-public class AppleAwsClientFactory implements AwsClientFactory {
+public class AppleAwsClientFactory implements AwsClientFactory, KryoSerializable {
 
-  public static final String AWS_REGION = "aws-region";
-  /** Proxy Host for redirecting AWS Requests, for Mezu this should be set to dps.iso.apple.com */
-  public static final String PROXY_HOST = "proxy-host";
-  /** Proxy Port for redirecting AWS Requests, Defaults to 443 which is the correct port for Mezu */
-  public static final String PROXY_PORT = "proxy-port";
+  public static final String AWS_REGION = "aws_region";
 
-  // Properties to let us pick up S3A configuration which serves the same purpose as the above proxy
-  // configs
-  private static final String S3A_PROXY_HOST = "fs.s3a.proxy.host";
-  private static final String S3A_PROXY_PORT = "fs.s3a.proxy.port";
+  static final String S3A_PROXY_HOST = "fs.s3a.proxy.host";
+  static final String S3A_PROXY_PORT = "fs.s3a.proxy.port";
+  static final String S3A_PROXY_SECURED = "fs.s3a.proxy.ssl.enabled";
+  static final String S3A_CREDENTIAL_PROVIDER = "fs.s3a.aws.credentials.provider";
+  static final String S3A_STS_REGION = "fs.s3a.assumed.role.sts.endpoint.region";
+
+  private static final String HTTP = "http://";
+  private static final String HTTPS = "https://";
 
   private static final Logger LOG = LoggerFactory.getLogger(AppleAwsClientFactory.class);
-
-  private static final Map<String, String> constantsKeyMap =
-      ImmutableList.of(
-              CredentialsConstants.AC_USERNAME_KEY,
-              CredentialsConstants.AC_PASSWORD_KEY,
-              CredentialsConstants.AC_TOTP_KEY,
-              CredentialsConstants.AC_DEVICEID_KEY,
-              CredentialsConstants.AC_AWS_ROLE_KEY,
-              CredentialsConstants.AC_ACCOUNT_ID_KEY,
-              CredentialsConstants.AC_CREDENTIALS_EARLYEXPIRYDELTA_KEY,
-              CredentialsConstants.AC_CACHESERVER_HOST_KEY,
-              CredentialsConstants.AC_CACHESERVER_PORT_KEY,
-              CredentialsConstants.AC_TLS_CA_CHAIN_PATH_KEY,
-              CredentialsConstants.AC_TLS_CERT_PATH_KEY,
-              CredentialsConstants.AC_TLS_PVTKEY_PATH_KEY,
-              CredentialsConstants.AC_TLS_CA_CHAIN_KEY,
-              CredentialsConstants.AC_TLS_CERT_KEY,
-              CredentialsConstants.AC_TLS_PVTKEY_KEY,
-              CredentialsConstants.AC_TLS_GROUPID_KEY,
-              CredentialsConstants.AC_TLS_CERT_VALIDITY_KEY,
-              CredentialsConstants.S3_PROXY_HOST,
-              CredentialsConstants.S3_PROXY_PORT,
-              CredentialsConstants.S3_CONNECTION_SSL_ENABLED,
-              CredentialsConstants.AC_CREDENTIALS_MAX_TRY_ATTEMPTS,
-              CredentialsConstants.ASSUMED_ROLE_STS_ENDPOINT_REGION_KEY,
-              CredentialsConstants.ASSUMED_ROLE_ARN_KEY,
-              CredentialsConstants.AC_IDENTITY_CERT_KEY_PATH_KEY,
-              CredentialsConstants.AC_IDENTITY_CERT_PATH_KEY)
-          .stream()
-          .collect(Collectors.toMap(k -> k.toLowerCase(Locale.ROOT), Function.identity()));
 
   private static final List<Set<String>> REQUIRED_KEYS =
       ImmutableList.of(
@@ -121,25 +99,32 @@ public class AppleAwsClientFactory implements AwsClientFactory {
               CredentialsConstants.AC_AWS_ROLE_KEY,
               CredentialsConstants.AC_ACCOUNT_ID_KEY));
 
+  private static final int PROXY_PORT_DEFAULT = 443;
+  private static final boolean PROXY_SECURED_DEFAULT = false;
+
   // This will be serialized to executors in spark so the provider will need to be reinitialized
   private transient AwsCredentialsProvider lazyProvider;
 
-  private static SdkHttpClient lazyHttpClient;
+  private transient SdkHttpClient lazyHttpClient;
 
-  // Keep the configuration for generating new credential providers
-  private SerializableConfiguration configuration;
+  private SerializableConfiguration lazyConfig;
+  private Map<String, String> properties;
 
+  @VisibleForTesting
   Configuration config() {
-    return configuration.get();
+    if (lazyConfig == null) {
+      Configuration conf = SparkSession.active().sessionState().newHadoopConf();
+      properties.entrySet().forEach(entry -> conf.set(entry.getKey(), entry.getValue()));
+      lazyConfig = new SerializableConfiguration(conf);
+    }
+
+    return lazyConfig.get();
   }
 
   @VisibleForTesting
   DynConstructors.Ctor<AWSAppleCredentialsProviderBase> getCredentialProviderConstructor() {
-    Configuration conf = configuration.get();
-
     String credentialProviderClassName =
-        conf.get(
-            "fs.s3a.aws.credentials.provider", AWSAppleConnectCredentialsProvider.class.getName());
+        config().get(S3A_CREDENTIAL_PROVIDER, AWSAppleConnectCredentialsProvider.class.getName());
 
     LOG.info("Setting up S3FileIO with CredentialProvider {}", credentialProviderClassName);
     return DynConstructors.builder(AWSAppleCredentialsProviderBase.class)
@@ -155,7 +140,7 @@ public class AppleAwsClientFactory implements AwsClientFactory {
     try {
       DynConstructors.Ctor<AWSAppleCredentialsProviderBase> ctor =
           getCredentialProviderConstructor();
-      AWSAppleCredentialsProviderBase provider = ctor.invoke(null, configuration.get());
+      AWSAppleCredentialsProviderBase provider = ctor.invoke(null, config());
       lazyProvider = new CredentialProviderWrapper(provider);
     } catch (Exception e) {
       throw new RuntimeException("Could not create Apple Aws Credentials Cache Provider", e);
@@ -165,11 +150,7 @@ public class AppleAwsClientFactory implements AwsClientFactory {
   }
 
   private <T extends AwsClientBuilder> T maybeApplyRegion(T builder) {
-    String region =
-        config()
-            .get(
-                CredentialsConstants.ASSUMED_ROLE_STS_ENDPOINT_REGION_KEY,
-                config().get(AWS_REGION));
+    String region = config().get(AWS_REGION, config().get(S3A_STS_REGION));
 
     if (region != null) {
       return (T) builder.region(Region.of(region));
@@ -179,12 +160,18 @@ public class AppleAwsClientFactory implements AwsClientFactory {
 
   @VisibleForTesting
   ProxyConfiguration getProxy() {
-    String proxyHost = config().get(PROXY_HOST, config().get(S3A_PROXY_HOST));
+    String proxyHost = config().get(S3A_PROXY_HOST);
+
     if (proxyHost != null) {
-      int proxyPort = config().getInt(PROXY_PORT, config().getInt(S3A_PROXY_PORT, 443));
+      int proxyPort = config().getInt(S3A_PROXY_PORT, PROXY_PORT_DEFAULT);
+
+      boolean proxySecured = config().getBoolean(S3A_PROXY_SECURED, PROXY_SECURED_DEFAULT);
+
+      String scheme = proxySecured ? HTTPS : HTTP;
+
       // ProxyConfig will ignore everything but host and port
       return ProxyConfiguration.builder()
-          .endpoint(URI.create("https://" + proxyHost + ":" + proxyPort))
+          .endpoint(URI.create(scheme + proxyHost + ":" + proxyPort))
           .build();
     }
     return null;
@@ -238,19 +225,15 @@ public class AppleAwsClientFactory implements AwsClientFactory {
   }
 
   @Override
-  public void initialize(Map<String, String> properties) {
-    Configuration newConfig = SparkSession$.MODULE$.active().sessionState().newHadoopConf();
-
-    // The Catalog passes through case-insensitive properties which are lower cased here, we need to
-    // revert them
-    properties.forEach((k, v) -> newConfig.set(constantsKeyMap.getOrDefault(k, k), v));
+  public void initialize(Map<String, String> props) {
+    this.properties = props;
 
     List<List<String>> missingRequired =
         REQUIRED_KEYS.stream()
             .map(
                 keyset ->
                     keyset.stream()
-                        .filter(key -> newConfig.get(key) == null)
+                        .filter(key -> config().get(key) == null)
                         .collect(Collectors.toList()))
             .collect(Collectors.toList());
 
@@ -261,11 +244,42 @@ public class AppleAwsClientFactory implements AwsClientFactory {
         String.format(
             "Cannot initialize AppleAwsClientFactory missing properties:\n%s",
             missingRequired.stream().map(List::toString).collect(Collectors.joining("\n OR \n"))));
-
-    configuration = new SerializableConfiguration(newConfig);
   }
 
-  class CredentialProviderWrapper implements AwsCredentialsProvider {
+  @Override
+  public void write(Kryo kryo, Output output) {
+    ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+    DataOutputStream outputStream = new DataOutputStream(byteOutputStream);
+
+    try {
+      config().write(outputStream);
+      outputStream.close();
+    } catch (IOException e) {
+      throw new RuntimeException("Could not Kryo Serialize Configuration", e);
+    }
+
+    kryo.writeObject(output, byteOutputStream.toByteArray());
+    kryo.writeClassAndObject(output, Maps.newHashMap(properties));
+  }
+
+  @Override
+  public void read(Kryo kryo, Input input) {
+    byte[] inputData = kryo.readObject(input, byte[].class);
+    DataInputStream inputStream = new DataInputStream(new ByteArrayInputStream(inputData));
+
+    Configuration conf = new Configuration(false);
+    try {
+      conf.readFields(inputStream);
+      inputStream.close();
+    } catch (IOException e) {
+      throw new RuntimeException("Could not Kryo Serialize Configuration", e);
+    }
+
+    this.lazyConfig = new SerializableConfiguration(conf);
+    this.properties = (Map<String, String>) kryo.readClassAndObject(input);
+  }
+
+  static class CredentialProviderWrapper implements AwsCredentialsProvider {
     private final AWSAppleCredentialsProviderBase v1Provider;
 
     CredentialProviderWrapper(AWSAppleCredentialsProviderBase v1Provider) {
