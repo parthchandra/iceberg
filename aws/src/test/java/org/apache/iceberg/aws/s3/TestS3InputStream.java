fa1f79a2eec71827f5a8e68af71926389f629200
Anil Khadka <akhadka@apple.com>
2025-04-11 12:40:07 -0700

    [Backport] Retry reading s3 inputstream (#1526)
    * API, AWS: Retry S3InputStream reads (#10433)
    
    Co-authored-by: Jack Ye <yzhaoqin@amazon.com>
    Co-authored-by: Xiaoxuan Li <xioxuan@amazon.com>
    (cherry picked from commit c0d73f4ef5c16401bdfd62e1745faf2fbbf62177)
    
    * AWS: Fix S3InputStream retry policy (#11335)
    
    (cherry picked from commit bbbfd1e2f14e62703d2dc5f1cb95631ab10f6d72)
    
    ---------
    
    Co-authored-by: Amogh Jahagirdar <amoghj@apache.org>
    Co-authored-by: Edgar Rodriguez <edgar.rodriguez@airbnb.com>

--- aws/src/test/java/org/apache/iceberg/aws/s3/TestS3InputStream.java
+++ aws/src/test/java/org/apache/iceberg/aws/s3/TestS3InputStream.java
@@ -51,16 +51,22 @@ public class TestS3InputStream {
 
   @Test
   public void testRead() throws Exception {
+    testRead(s3);
+  }
+
+  S3InputStream newInputStream(S3Client s3Client, S3URI uri) {
+    return new S3InputStream(s3Client, uri);
+  }
+
+  protected void testRead(S3Client s3Client) throws Exception {
     S3URI uri = new S3URI("s3://bucket/path/to/read.dat");
     int dataSize = 1024 * 1024 * 10;
     byte[] data = randomData(dataSize);
 
     writeS3Data(uri, data);
 
-    try (SeekableInputStream in = new S3InputStream(s3, uri)) {
+    try (SeekableInputStream in = newInputStream(s3Client, uri)) {
       int readSize = 1024;
-      byte[] actual = new byte[readSize];
-
       readAndCheck(in, in.getPos(), readSize, data, false);
       readAndCheck(in, in.getPos(), readSize, data, true);
 
@@ -109,6 +115,10 @@ public class TestS3InputStream {
 
   @Test
   public void testRangeRead() throws Exception {
+    testRangeRead(s3);
+  }
+
+  protected void testRangeRead(S3Client s3Client) throws Exception {
     S3URI uri = new S3URI("s3://bucket/path/to/range-read.dat");
     int dataSize = 1024 * 1024 * 10;
     byte[] expected = randomData(dataSize);
@@ -120,7 +130,7 @@ public class TestS3InputStream {
 
     writeS3Data(uri, expected);
 
-    try (RangeReadable in = new S3InputStream(s3, uri)) {
+    try (RangeReadable in = newInputStream(s3Client, uri)) {
       // first 1k
       position = 0;
       offset = 0;
@@ -152,7 +162,7 @@ public class TestS3InputStream {
   @Test
   public void testClose() throws Exception {
     S3URI uri = new S3URI("s3://bucket/path/to/closed.dat");
-    SeekableInputStream closed = new S3InputStream(s3, uri);
+    SeekableInputStream closed = newInputStream(s3, uri);
     closed.close();
     Assertions.assertThatThrownBy(() -> closed.seek(0))
         .isInstanceOf(IllegalStateException.class)
@@ -161,12 +171,16 @@ public class TestS3InputStream {
 
   @Test
   public void testSeek() throws Exception {
+    testSeek(s3);
+  }
+
+  protected void testSeek(S3Client s3Client) throws Exception {
     S3URI uri = new S3URI("s3://bucket/path/to/seek.dat");
     byte[] expected = randomData(1024 * 1024);
 
     writeS3Data(uri, expected);
 
-    try (SeekableInputStream in = new S3InputStream(s3, uri)) {
+    try (SeekableInputStream in = newInputStream(s3Client, uri)) {
       in.seek(expected.length / 2);
       byte[] actual = new byte[expected.length / 2];
       IOUtil.readFully(in, actual, 0, expected.length / 2);
@@ -197,5 +211,9 @@ public class TestS3InputStream {
     } catch (BucketAlreadyExistsException e) {
       // don't do anything
     }
+  }
+
+  protected S3Client s3Client() {
+    return s3;
   }
 }
