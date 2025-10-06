b19bf7ad950d65b8b4cf5c07142d4494bc60ce15
Parth Chandra <parthc@apple.com>
2025-10-06 15:12:53 -0700

    fix build

--- spark/v3.4/spark/src/main/java/org/apache/iceberg/spark/data/vectorized/CometColumnarBatchReader.java
+++ spark/v3.4/spark/src/main/java/org/apache/iceberg/spark/data/vectorized/CometColumnarBatchReader.java
@@ -25,7 +25,6 @@ import java.util.Map;
 import org.apache.comet.CometRuntimeException;
 import org.apache.comet.parquet.AbstractColumnReader;
 import org.apache.comet.parquet.IcebergCometBatchReader;
-import org.apache.comet.parquet.RowGroupReader;
 import org.apache.comet.vector.CometSelectionVector;
 import org.apache.comet.vector.CometVector;
 import org.apache.iceberg.Schema;
@@ -73,7 +72,6 @@ class CometColumnarBatchReader implements VectorizedReader<ColumnarBatch> {
     this.delegate = new IcebergCometBatchReader(readers.size(), SparkSchemaUtil.convert(schema));
   }
 
-  @Override
   public void setRowGroupInfo(
       PageReadStore pageStore, Map<ColumnPath, ColumnChunkMetaData> metaData, long rowPosition) {
     setRowGroupInfo(pageStore, metaData);
