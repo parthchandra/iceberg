c49b25e6de5c2d530d760dac2f8dfa9849eba247
Parth Chandra <parthc@apple.com>
2025-10-21 10:10:32 -0700

    enable comet_native in tests

--- spark/v3.5/spark-extensions/src/test/java/org/apache/iceberg/spark/extensions/ExtensionsTestBase.java
+++ spark/v3.5/spark-extensions/src/test/java/org/apache/iceberg/spark/extensions/ExtensionsTestBase.java
@@ -62,7 +62,7 @@ public abstract class ExtensionsTestBase extends CatalogTestBase {
                 "spark.shuffle.manager",
                 "org.apache.spark.sql.comet.execution.shuffle.CometShuffleManager")
             .config("spark.comet.explainFallback.enabled", "true")
-            .config("spark.sql.iceberg.parquet.reader-type", "COMET")
+            .config("spark.sql.iceberg.parquet.reader-type", "COMET_NATIVE")
             .config("spark.memory.offHeap.enabled", "true")
             .config("spark.memory.offHeap.size", "10g")
             .config("spark.comet.use.lazyMaterialization", "false")
--- spark/v3.5/spark/src/test/java/org/apache/iceberg/spark/source/TestFilteredScan.java
+++ spark/v3.5/spark/src/test/java/org/apache/iceberg/spark/source/TestFilteredScan.java
@@ -116,6 +116,16 @@ public class TestFilteredScan {
         SparkSession.builder()
             .master("local[2]")
             .config("spark.driver.host", InetAddress.getLoopbackAddress().getHostAddress())
+            .config("spark.plugins", "org.apache.spark.CometPlugin")
+            .config(
+                "spark.shuffle.manager",
+                "org.apache.spark.sql.comet.execution.shuffle.CometShuffleManager")
+            .config("spark.comet.explainFallback.enabled", "true")
+            .config("spark.sql.iceberg.parquet.reader-type", "COMET_NATIVE")
+            .config("spark.memory.offHeap.enabled", "true")
+            .config("spark.memory.offHeap.size", "10g")
+            .config("spark.comet.use.lazyMaterialization", "false")
+            .config("spark.comet.schemaEvolution.enabled", "true")
             .getOrCreate();
   }
 
